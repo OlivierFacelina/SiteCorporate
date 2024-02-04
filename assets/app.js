38e80e50a23bd83c6f3667058611d7ac66f93663
OlivierF <olivier.facelina@gmail.com>
2024-02-04 22:20:48 +0100

    Last commit

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,5 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
-import './js/script.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
