48cf6631fc08f2ec257a34a3e9954e521a6e69a1
OlivierF <olivier.facelina@gmail.com>
2024-02-05 09:14:45 +0100

    Update environment

--- public/assets/@symfony/stimulus-bundle/controllers-9d42643c079ab11f27a3a9614f81cc2f.js
+++ public/assets/@symfony/stimulus-bundle/controllers-9d42643c079ab11f27a3a9614f81cc2f.js
@@ -2,4 +2,4 @@ import controller_0 from "../ux-turbo/turbo_controller.js";
 import controller_1 from "../../controllers/hello_controller.js";
 export const eagerControllers = {"symfony--ux-turbo--turbo-core": controller_0, "hello": controller_1};
 export const lazyControllers = {};
-export const isApplicationDebug = false;
+export const isApplicationDebug = true;
